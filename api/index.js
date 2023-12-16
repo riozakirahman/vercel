376c6265f53dd523fd4e596f0ad5928e5ba40d85
Rio Zakirahman <rio.zakirahman@binus.ac.id>
2023-12-16 18:17:23 +0700

    initial commit

---
+++ api/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv");
+const cors = require("cors");
+dotenv.config();
+
+app.use(cors());
+app.get("/api/test", (req, res) => {
+  res.json(`Hello World ${Date.now()}`);
+});
+if (process.env.API_PORT) {
+  app.listen(process.env.API_PORT);
+}
+
+module.exports = app;
--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [result, setResult] = useState("");
+
+  const fetchInfo = async () => {
+    const url = `${process.env.REACT_APP_API_URL}/test`;
+    const response = await fetch(url);
+    const data = await response.json();
+    setResult(data);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={fetchInfo}>Fetch Data From Api</button>
+      <p>result: {result}</p>
     </div>
   );
 }
